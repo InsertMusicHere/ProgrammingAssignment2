e6ccf2442eccaa4f38425607b5d7758daffaaf3a
InsertMusicHere <smtrlive27@gmail.com>
2020-07-19 17:23:55 +0530

    Part of Course - R programming from Coursera - Week 3 assignment
    Function to create matrix object to cache its inverse
    
    chagned 'm' to 'I' and setmean and getmean to setinverse and getinverse from original/provided data
    
    changed x=numeric() to matrix()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##Part of Course - R programming from Coursera - Week 3 assignment 
 
+##Function to create matrix object to cache its inverse
+
+##chagned 'm' to 'I' and setmean and getmean to setinverse and getinverse
+##from original/provided data
+##changed x=numeric() to matrix()
 makeCacheMatrix <- function(x = matrix()) {
-
+  I <- NULL
+  set <- function(y) {
+    x <<- y
+    I <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) I <<- inverse
+  getInverse <- function() I
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  I <- x$getInverse() ##x$getmean() changed to x$getInverse()
+  if (!is.null(I)) {
+    message("getting cached data")
+    return(I)
+  }
+  mat <- x$get()
+  I <- solve(mat, ...)
+  x$setInverse(I)
+  I
 }
